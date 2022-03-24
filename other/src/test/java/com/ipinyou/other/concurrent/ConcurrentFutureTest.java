2e881fe06c67772dcbcecda6b08ef68c6150736a
wentai <wentai.wang@ipinyou.com>
2022-03-24 14:38:34 +0800

    提交3

--- other/src/test/java/com/ipinyou/other/concurrent/ConcurrentFutureTest.java
+++ other/src/test/java/com/ipinyou/other/concurrent/ConcurrentFutureTest.java
@@ -18,6 +18,7 @@ public class ConcurrentFutureTest {
     public void test1() throws InterruptedException {
         System.out.println("提交1");
         System.out.println("提交2");
+        System.out.println("提交3");
         StopWatch stopWatch = new StopWatch();
         stopWatch.start(String.valueOf(Thread.currentThread().getId()));
         log.info("{}主线程启动", Thread.currentThread().getId());
